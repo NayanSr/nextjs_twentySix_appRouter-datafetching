c7576adfd696098a892698b7cde15397155674e9
NayanSr <nayankbps@gmail.com>
2023-10-13 20:24:59 +0600

    'revalidate-ISRS'

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,21 @@
 import Products from "@/components/UI/Products";
 
-export const getData = async () => {
+/* export const getData = async () => {
   const res = await fetch("http://localhost:5000/watches", {
     cache: "force-cache",
   });
   const data = await res.json();
   return data;
-};
+}; */
 
 const HomePage = async () => {
-  // const res = await fetch("http://localhost:5000/watches", {
-  //   cache: "force-cache",
-  // });
-  // const data = await res.json();
+  const res = await fetch("http://localhost:5000/watches", {
+    cache: "force-cache",
+    next: { revalidate: 5 }, //! ISRS
+  });
+  const data = await res.json();
   // console.log(data);
-  const data = await getData();
+  // const data = await getData();
   return (
     <div>
       <h1 className="text-4xl text-center my-10">Welcome To Watch Gallery</h1>
