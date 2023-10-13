9814fa60c88bb2b8d9522c3649621a6832ee5192
NayanSr <nayankbps@gmail.com>
2023-10-13 20:30:22 +0600

    'not-found-page'

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <p className="text-6xl">404 Page Not Found............</p>
+    </div>
+  );
+}
