77f275ef352d076e8f5e732b76413559b5932708
NayanSr <nayankbps@gmail.com>
2023-10-13 20:18:37 +0600

    'data-fetching'

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
-const HomePage = () => {
+import Products from "@/components/UI/Products";
+
+export const getData = async () => {
+  const res = await fetch("http://localhost:5000/watches", {
+    cache: "force-cache",
+  });
+  const data = await res.json();
+  return data;
+};
+
+const HomePage = async () => {
+  // const res = await fetch("http://localhost:5000/watches", {
+  //   cache: "force-cache",
+  // });
+  // const data = await res.json();
+  // console.log(data);
+  const data = await getData();
   return (
     <div>
       <h1 className="text-4xl text-center my-10">Welcome To Watch Gallery</h1>
-      <div className="col-span-9 grid grid-cols-3 gap-5 p-10 w-[80%] mx-auto"></div>
+      <div className="col-span-9 grid grid-cols-3 gap-5 p-10 w-[80%] mx-auto">
+        {data.map((pd) => (
+          <Products key={pd.id} product={pd} />
+        ))}
+      </div>
     </div>
   );
 };
